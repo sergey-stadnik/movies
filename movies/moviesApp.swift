961e73212d2981658147bd9ad9f0205b5514fdaa
Sergii Stadnyk <>
2023-06-15 12:24:03 +0300

    in com

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Sergii Stadnyk on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
