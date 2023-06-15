ada460eb8aa937b9302a1efd64e1eef8cddb081f
Sergii Stadnyk <>
2023-06-15 17:49:36 +0300

    Detailed view implemented

--- movies/Views/MovieDetailView.swift
+++ movies/Views/MovieDetailView.swift
@@ -13,8 +13,35 @@ struct MovieDetailView<ViewModelType: MovieDetailViewModelProtocol>: View {
     @State private var showingAlert = false
 
     var body: some View {
-        VStack {
-            Text(viewModel.movie.name)
+        ScrollView {
+            VStack {
+                Text(viewModel.movie.name)
+                    .font(.system(.largeTitle))
+                AsyncImage(url: URL(string: viewModel.movie.image)) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .cornerRadius(10)
+
+
+                    Text(viewModel.movie.meta)
+                        .padding(.top, 20)
+
+                    Text(viewModel.movie.rating)
+
+                    Text(viewModel.movie.synopsis)
+                        .font(.system(.body))
+                        .padding()
+
+                    Text("$\(viewModel.movie.price)")
+                        .font(.system(.largeTitle))
+                        .padding(.top, 20)
+                        .foregroundColor(.red)
+
+                } placeholder: {
+                    ProgressView()
+                }
+            }
         }
         .alert(viewModel.errorMessage ?? "", isPresented: $viewModel.isAlertPresented) {
             Button("OK", role: .cancel) {
